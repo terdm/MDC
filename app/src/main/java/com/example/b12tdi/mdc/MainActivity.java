7c766b6b4452c6d9613f18190a42c1812b51a150
terdm <terdm@yandex.ru>
2018-04-15 13:56:43 +0300

    back

--- app/src/main/java/com/example/b12tdi/mdc/MainActivity.java
+++ app/src/main/java/com/example/b12tdi/mdc/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     //AsyncT asyncT;
+    String refreshedToken;
+
+    TextView myAwesomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +23,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d("MyTag","MainActivity onCreate starts ");
         setContentView(R.layout.activity_main);
+        refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Firebase", "token "+ refreshedToken);
+        myAwesomeTextView = (TextView)findViewById(R.id.myAwesomeTextView);
+        myAwesomeTextView.setText(refreshedToken);
+
         //asyncT = new AsyncT();
     }
 
     public void onButtonLog(View v) {
         Log.d("MyTag","onButtonLog starts ");
-        String[] cmd = new String[] { "logcat", "-f", "/storage/extSdCard/log/MyTag", "-v", "time", "ActivityManager:W", "myapp:D" };
+        //String[] cmd = new String[] { "logcat", "-f", "/storage/extSdCard/log/MyTag", "-v", "time", "ActivityManager:W", "myapp:D" };
+        String[] cmd = new String[] { "logcat", "-f", "/storage/emulated/0/log/MyTag", "-v", "time", "ActivityManager:W", "myapp:D" };
         try {
             Runtime.getRuntime().exec(cmd);
         }
