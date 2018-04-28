8726cffd3b096d148f72e682d38f041bee198c79
terdm <terdm@yandex.ru>
2018-04-28 07:30:14 +0300

    home

--- app/src/main/java/com/example/b12tdi/mdc/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/b12tdi/mdc/MyFirebaseMessagingService.java
@@ -26,6 +26,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -66,13 +67,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            sendBroadcastMessage(remoteMessage.getData().toString());
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendBroadcastMessage(remoteMessage.getNotification().getBody());
         }
-        sendBroadcastMessage(remoteMessage.getNotification().getBody());
+
 
         /*
         //этот кусок взят из messaging. здесь не предполагается запуск детектора
